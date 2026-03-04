dfdc815206665085a166b3f01cc153d0c0043cf8
Yeonho Jang <siabard@gmail.com>
2026-03-04 14:16:13 +0900

    백엔드 구성

---
+++ src/main/backend/core.clj
@@ -0,0 +1,34 @@
+(ns backend.core
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :as response]
+            [reitit.ring :as ring]))
+
+
+;; 1. API Router설정
+
+(def app
+  (ring/ring-handler
+   (ring/router
+    ["/api/hello" {:get (fn [_] (response/response "HELLO. I'm API4"))}])
+   (ring/create-default-handler)))
+
+;; 2. 서버 인스탄스 관리
+(defonce server (atom nil))
+
+(defn start-server []
+  (when-not @server
+    (println "백엔드 서버 포트 3000 에서 시작")
+    (reset! server (jetty/run-jetty #'app {:port 3000 :join? false}))))
+
+(defn stop-server []
+  (when @server
+    (println "서버 중단")
+    (.stop @server)
+    (reset! server nil)))
+
+(defn restart-server []
+  (stop-server)
+  (start-server))
+
+
+(start-server)
